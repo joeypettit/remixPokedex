906fd99a98aff11e4b25b94fb62981e4b2c57471
Joey Pettit <pettitbass@gmail.com>
2023-03-30 11:19:55 -0500

    built pokemon sqlite database

--- prisma/pokeGrabber.js
+++ prisma/pokeGrabber.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function buildOneFiftyOriginal() {
-  for (let i = 1; i <= 3; i++) {
+  for (let i = 1; i <= 150; i++) {
     let pokePromise = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
     let thisPokemon = await pokePromise.json();
 
@@ -20,14 +20,24 @@ async function buildOneFiftyOriginal() {
         if (url[searchIndex] === "/") {
           console.log("char is", url[searchIndex]);
           searchingForFirstSlash = false;
-          abilityId = url.slice(searchIndex + 1).slice(0, -1);
+          abilityId = Number(url.slice(searchIndex + 1).slice(0, -1));
         } else {
           console.log("char is", url[searchIndex]);
           searchIndex--;
         }
       }
 
-      abilityIds.push(abilityId);
+      await prisma.ability.upsert({
+        where: { id: abilityId },
+        create: {
+          id: abilityId,
+          name: ability.ability.name,
+          url: ability.ability.url,
+        },
+        update: {},
+      });
+
+      abilityIds.push({ id: abilityId });
     }
 
     // create array of move ids from moves array
@@ -41,38 +51,50 @@ async function buildOneFiftyOriginal() {
       let searchIndex = url.length - 2;
       while (searchingForFirstSlash) {
         if (url[searchIndex] === "/") {
-          console.log("char is", url[searchIndex]);
           searchingForFirstSlash = false;
-          moveId = url.slice(searchIndex + 1).slice(0, -1);
+          moveId = Number(url.slice(searchIndex + 1).slice(0, -1));
         } else {
-          console.log("char is", url[searchIndex]);
           searchIndex--;
         }
       }
 
-      moveIds.push(moveId);
+      await prisma.move.upsert({
+        where: { id: moveId },
+        create: {
+          id: moveId,
+          name: move.move.name,
+          url: move.move.url,
+        },
+        update: {},
+      });
+
+      moveIds.push({ id: moveId });
     }
 
     await prisma.pokemon.create({
       data: {
-        pokeId: i,
+        id: i,
         name: thisPokemon.name,
         height: thisPokemon.height,
         weight: thisPokemon.weight,
         spriteUrl: thisPokemon.sprites.front_default,
+        abilities: {
+          connect: abilityIds,
+        },
+        moves: {
+          connect: moveIds,
+        },
       },
     });
   }
 }
 
-// buildOneFiftyOriginal()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
-
-buildOneFiftyOriginal();
+buildOneFiftyOriginal()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
